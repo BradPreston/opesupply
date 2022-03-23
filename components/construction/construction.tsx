ef5b79149308a31f288e4ea125ebe677ff49e0af
bradpreston <bap5393@gmail.com>
2022-03-22 23:27:19 -0400

    initial commit

---
+++ components/construction/construction.tsx
@@ -0,0 +1,14 @@
+import styles from './construction.module.scss';
+
+const Construction = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h1 data-heading="Ope Supply">
+		<span contentEditable data-heading="Ope Supply">Ope Supply</span>
+	</h1>
+      <p>is currently under construction</p>
+    </section>
+  )
+}
+
+export default Construction;
