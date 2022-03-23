f457578f80a26073b39180473bfb2bd559147d10
bradpreston <bap5393@gmail.com>
2022-03-23 07:39:41 -0400

    removed content editable on span

--- components/construction/construction.tsx
+++ components/construction/construction.tsx
@@ -4,7 +4,7 @@ const Construction = () => {
   return (
     <section className={styles.wrapper}>
       <h1 data-heading="Ope Supply">
-		<span contentEditable data-heading="Ope Supply">Ope Supply</span>
+		<span data-heading="Ope Supply">Ope Supply</span>
 	</h1>
       <p>is currently under construction</p>
     </section>
