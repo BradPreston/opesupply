c449fa03860185045df38002819d5b1ed36eaea0
bradpreston <bap5393@gmail.com>
2022-03-23 07:53:09 -0400

    mobile friendly vh

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react';
 import Construction from '../components/construction/construction';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      document.querySelector('main')?.style.setProperty('--vh', window.innerHeight/100 + 'px');
+    })
+  }, []);
   return (
     <main>
       <Construction />
